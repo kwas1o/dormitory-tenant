f7662c3b6ec5749de6f4f63b8cf33761da853d74
kwas1o <watthana.s@kkumail.com>
2024-09-06 23:20:49 +0700

    Update package dependencies and fix sidebar height issue, home page structure

--- frontend-tenant/src/App.jsx
+++ frontend-tenant/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <div className="app">
-        <Sidebar />
+        {/* <Sidebar /> */}
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
--- frontend-tenant/src/pages/Home.jsx
+++ frontend-tenant/src/pages/Home.jsx
@@ -1,17 +1,33 @@
 import React from 'react';
 import Sidebar from '../componants/sidebar/sidebar';
+import '../css/home.css';
 
 const Home = () => {
     return (
-        <div>
-            {/* Sidebar */}
-            <div className="sidebar">
+        <div className='home-container'>
+            <div className="h-sidebar">
                 <Sidebar />
             </div>
 
             {/* Main content */}
-            <div className="main-content">
-                {/* Main content goes here */}
+            <div className="h-main-content">
+               <div className="h-mt-content">
+                <p className='h-hello-mes'>สวัสดี</p>
+                <p className='h-username'>คุณ วรรธนะ เสี้ยมแหลม</p>
+               </div>
+               <div className="h-mbt-content">
+                <div className="h-mbtl-content">
+                    <div className="h-text-header-l">
+                        <p className='h-bill-label'>บิลล์ของฉัน</p>
+                        <p className='h-years-label'>ปี 2567</p>
+                    </div>
+                </div>
+                <div className="h-mbtr-content">
+                <div className="h-text-header-r">
+                        <p className='h-detail-label'>รายละเอียด</p>
+                    </div>
+                </div>
+               </div>
             </div>
         </div>
     );
